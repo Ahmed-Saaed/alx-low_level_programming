8f341be51d3a581472ab091a56360ba75b47af0c
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-15 15:43:40 -0700

    my first c snippt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main ~ is the essintial function
+ *
+ * Description: A program that display a sentence using puts
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
