5b5397215f5ea3424f0c41ad8ed2c4b4087084dd
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-15 16:30:52 -0700

    thanks betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,17 +3,17 @@
 /**
  * main - Entry point
  *
- * Description: a c program 
+ * Description: a c program to use a built in function
  *
  * Return: Always 0 (Success)
- * */
+*/
 
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n",sizeof(char))
-	printf("size of an int: %lu byte(s)\n",sizeof(int))
-	printf("size of a long int: %lu byte(s)\n",sizeof(long int))
-	printf("size of a long long int: %lu byte(s)\n",sizeof(long long int))
-	printf("size of a float: %lu byte(s)\n",sizeof(float))
-	return (0)
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
