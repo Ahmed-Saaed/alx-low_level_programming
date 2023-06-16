60c0f80c5f2b6e4e6b61eb10ad99c9755e9b38b8
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 14:51:14 -0700

    what happend to dry

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = 'a';
+	char C = "A";
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+
+	putchar('\n');
+	return (0);
+}
