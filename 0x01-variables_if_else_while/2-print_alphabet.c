97bf87b165be11871be1137c9d9a0d72848a9137
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 11:26:38 -0700

    putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program to display the alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = "a";
+
+	while (ch <= "z")
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar("\n");
+	return (0);
+}
