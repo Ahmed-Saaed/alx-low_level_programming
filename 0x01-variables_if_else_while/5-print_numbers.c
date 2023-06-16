f09f287992da2cf3bb79846d95a92d3dcb4be1b7
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 15:21:22 -0700

    loobs loobs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+
+	int nums;
+
+	for (nums = 0 ; nums <= 9 ; nums++)
+		printf("i", nums);
+	printf("\n");
+
+	return (0);
+}
