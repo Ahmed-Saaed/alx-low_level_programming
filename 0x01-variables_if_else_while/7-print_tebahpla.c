a8b9639d37e5572f3b5dd9d0409cd19c67f94840
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 15:44:40 -0700

    foor loobs , while loobs

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c = 'z'
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
