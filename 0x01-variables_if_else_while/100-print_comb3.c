00aa484af8430088b96ca6a70714bf01c4624170
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 16:43:14 -0700

    add 1st advanced task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+*
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1 + 48);
+				putchar(n2 + 48);
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	purchar('\n');
+
+	return (0);
+}
