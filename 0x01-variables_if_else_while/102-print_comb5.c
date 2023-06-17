b976dfa1ebc19683c36b27352d631bf8a1bbc9bb
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-16 18:39:07 -0700

    wshooo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 99; n1++)
+	{
+		for (n2 = 0; n2 <= 99; n2++)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (n1 != 99 ||  n2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
