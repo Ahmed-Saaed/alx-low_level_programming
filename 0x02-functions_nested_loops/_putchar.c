243c7493e642ebbb32fc495a0c790d90cb009a8d
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-20 17:19:05 -0700

    intial commit

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * _putchar - write c to the output
+ * @c: the character to display, a parameter
+ *
+ * Return: On Success 1.
+*/
+
+int _putchar(char c)
+{
+
+	return (write(1, &C, 1));
+}
