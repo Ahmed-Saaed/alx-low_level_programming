b1ba05b781e5c391695b9dc8179c20f718d7f600
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-20 19:51:58 -0700

    tic toc

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,11 @@ void times_table(void)
 {
 	int num, mul, prod;
 
-	for (num = 0; num <= 9; num ++)
+	for (num = 0; num <= 9; num++)
 	{
 		for (mul = 1 ; mul <= 9; mul++)
 		{
-			_putchar(',');
+			_putchar('.');
 			_putchar(' ');
 
 			prod = num * mul;
