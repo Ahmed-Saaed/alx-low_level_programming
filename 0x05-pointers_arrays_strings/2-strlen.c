3ca85f6474ad9c93a34cfe2a17f1a0c4ee01e625
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-26 09:18:27 -0700

    add task 1 & 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Return: ALways 0 is success
+ */
+
+int _strlen(char *s)
+{
+	for (int l = 0; *s++;)
+		a++;
+	return (a);
+}
