09adbd9f99eb716c13aabcc8e0e1728bc79802c8
Ahmed-Saaed <ahmedveso@gmail.com>
2023-06-29 09:52:33 -0700

    add task 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,55 +9,27 @@
  *  Return: 1, 0
 */
 
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-
-/**
- * isDelimiter - ascci delimiter
- *
- * @c: char
- *
- * Return: 1, 0
-*/
-
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = "\t\n,;.!?\"(){}";
-
-	for (i = 0; i < 12; i++)
-	{
-		if (c == delimiter[i])
-			return (1);
-		return (0);
-	}
-}
-
-/**
- * cap_string - capitalizes all words in a string
- * @s: string
- * Return: address of s
- */
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int l = 1;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	while (*s)
+	while (*(s + i))
 	{
-		if (isDelimiter(*s))
-			l = 1;
-		else if (isLower(*s) && l)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			*S -= 32;
-			l = 0;
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
 		}
-		else
-			l = 0;
-		s++;
+		i++;
 	}
-	return (ptr);
-
+	return (s);
 }
